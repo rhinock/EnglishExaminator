810675bfd424a5909148687fea22f5f5a67b61a0
VirtualFX <dev.evolution@yandex.ru>
2019-06-26 09:45:16 +0300

    Three forms core added (Includes File parser and Forms verifier)

---
+++ ConsoleApp3/Core/ThreeFormsParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Core
+{
+    class ThreeFormsParser
+    {
+        static List<(string, string, string)> ParseFile(string filename)
+        {
+            if (!File.Exists(filename))
+                throw new FileNotFoundException($"File {filename} is not exist");
+
+            List<(string, string, string)> rezultList = new List<(string, string, string)>();
+            string[] rows = File.ReadAllLines(filename);
+
+            foreach (string row in rows)
+            {
+                Regex wordPattern = new Regex(@"\w+");
+                MatchCollection matches = wordPattern.Matches(row);
+                if (matches.Count == 3)
+                {
+                    rezultList.Add((matches[0].Value, matches[1].Value, matches[2].Value));
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return rezultList;
+        }
+    }
+}
---
+++ ConsoleApp3/Core/ThreeFormsVerifier.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core
+{
+    public class ThreeFormsVerifier
+    {
+        private List<(string baseForm, string pastSimple, string pastPerfect)> _forms;
+        public List<(string baseForm, string pastSimple, string pastPerfect)> Forms { get => _forms; }
+
+        private List<(string baseForm, string pastSimple, string pastPerfect)> _updatedForms;
+        public List<(string baseForm, string pastSimple, string pastPerfect)> UpdatedForms { get => _updatedForms; }
+
+        public (string word, int index, int form) ChosenWord { get; protected set; }
+
+        public int Score { get; protected set; }
+
+        public ThreeFormsVerifier(List<(string baseForm, string pastSimple, string pastPerfect)> forms)
+        {
+            if (forms == null) throw new ArgumentNullException("Argument cannot be null");
+
+            _forms = forms;
+        }
+
+        public string GetRandomWord()
+        {
+            Random rand = new Random();
+            var form = rand.Next(3);
+            var index = rand.Next(_forms.Count);
+
+            switch (form)
+            {
+                case 0:
+                    ChosenWord = (_forms[index].baseForm, index, form);
+                    return ($"1. { _forms[index].baseForm }");
+                case 1:
+                    ChosenWord = (_forms[index].pastSimple, index, form);
+                    return ($"2. { _forms[index].pastSimple }");
+                case 2:
+                    ChosenWord = (_forms[index].pastPerfect, index, form);
+                    return ($"3. { _forms[index].pastPerfect }");
+            }
+            return null;
+        }
+
+        public (bool rezult, string text) CheckRezults(string input)
+        {
+            string[] split = input.Split(' ');
+            bool rezult = false;
+
+            if (split.Length != 3 || split.All(i => string.IsNullOrEmpty(i)))
+                return (false, _forms[ChosenWord.index].ToString());
+
+            UpdatedForms.Add((split[0], split[1], split[2]));
+
+            if (_forms[ChosenWord.index].Equals(_updatedForms.Last()))
+            {
+                rezult = true;
+                Score++;
+            }
+            else
+                rezult = false;
+
+            var correct = _forms[ChosenWord.index];
+            _forms.RemoveAt(ChosenWord.index);
+            return (rezult, correct.ToString());
+        }
+    }
+}
